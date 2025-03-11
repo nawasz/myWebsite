a943c5a330093e4dad8de70d863429ed06566a5e
Nawaz <nawaznz261970@gmail.com>
2025-03-11 16:27:22 +0530

    add

--- nexotra/src/components/Insights/BlogPost.jsx
+++ nexotra/src/components/Insights/BlogPost.jsx
@@ -8,15 +8,15 @@ import { Helmet } from "react-helmet";
 import img1 from "../../assets/blogs/Img_1.png";
 import img2 from "../../assets/blogs/Img_2.png";
 import img3 from "../../assets/blogs/Img_3.png";
-import img4 from "../../assets/blogs/Img_4.png";
-import img5 from "../../assets/blogs/Img_5.png";
+// import img2 from "../../assets/blogs/Img_4.png";
+// import img1 from "../../assets/blogs/Img_5.png";
 import Footer from "../footer";
 const imageMap = {
   1: img1,
   2: img2,
   3: img3,
-  4: img4,
-  5: img5,
+  4: img2,
+  5: img1,
 };
 
 const BlogPost = () => {
